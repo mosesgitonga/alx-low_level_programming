40dba73cc699ade01c61a264e4dc4b14ce541712
mosesgitonga <infosec947@gmail.com>
2023-02-16 04:39:51 -0800

    change-c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
+
 /** 
- * Description:main -  prints "Programming is like building a multilingual
- * puzzle, followed by a new line
- * Return: Always 0.
+ *Description:main-prints "Programming is like building a multilingual
+  puzzle, followed by a new line
+  Return: Always 0.
  */
+
+
 int main(void)
 {
-	puts ("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return(0);
-}
+M-x delete-trailing-whitespace}
