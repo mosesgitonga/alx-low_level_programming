77153c840cefa534293af99a2d6c3a98dd6b6c61
mosesgitonga <infosec947@gmail.com>
2023-02-16 03:24:15 -0800

    my c program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /** 
- * main .  print "Programming is like building
- * a multilingual puzzle
- * Return Always 0
+ * main -  prints "Programming is like building a multilingual
+ * puzzle, followed by a new line
+ * Return Always 0.
  */
 
 
