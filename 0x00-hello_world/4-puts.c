d0f011b4903333013ad23ea4ae5a6f52327d3346
mosesgitonga <infosec947@gmail.com>
2023-02-16 02:40:40 -0800

    coding in ac at alx

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
+/** 
+ * program to print "Programming is
+ *  like building a multilingual puzzle
+ */
+:WQ
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
