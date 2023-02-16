3d1021abe349e616fc0e4686087370b9d9b24fe6
mosesgitonga <infosec947@gmail.com>
2023-02-16 08:20:23 -0800

    e

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
-/*
- * main - Entry point
- * Description: - A program that prints a line using printf function
- * Return:0  (success)
+/**
+ * Description: main - Prints out a string to stdout.
+ * Return: 0 if success.
  */
 int main(void)
 {
