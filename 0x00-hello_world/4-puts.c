ad2d773cc6a9329e45c07e6d57013903a9314d76
mosesgitonga <infosec947@gmail.com>
2023-02-16 04:45:20 -0800

    c-program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /** 
- *Description:main-prints "Programming is like building a multilingual
-  puzzle, followed by a new line
-  Return: Always 0.
+ * Description:main -  prints "Programming is like building a multilingual
+ * puzzle, followed by a new line
+ * Return: Always 0.
  */
 
 
@@ -11,4 +11,4 @@ int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return(0);
-M-x delete-trailing-whitespace}
+}
