c561d7e840c0c0fcbb855e464a6b05b56745c4db
mosesgitonga <infosec947@gmail.com>
2023-02-21 10:27:23 -0800

    hel

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,16 +1,17 @@
 #include "main.h"
 /**
  * print_last_digit - print last
- * digit 
+ * digit
  *
- * @x: kbskks
+ * @n: kbskks
  * Return: 0
  */
 int print_last_digit(int n)
 {
 	int l;
 
-	l = n % 10; 
+	l = n % 10;
+
 	if (l < 0)
 	{
 		l = l * -1;
