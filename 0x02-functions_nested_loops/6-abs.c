2e1353d98b34fe350e06e415910cb57a06576213
mosesgitonga <infosec947@gmail.com>
2023-02-21 09:11:01 -0800

    correction

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,17 +2,17 @@
 #include <stdio.h>
 /**
  *  _abs - finds absolute value
- *
+ * @r: helps to do magic
  *  Return: 0
  */
 int _abs(int r)
-{	
+{
 	if (r < 0)
 	{
 		int abs;
+
 		abs = r * -1;
 		return (abs);
 	}
 	return (r);
 }
-
