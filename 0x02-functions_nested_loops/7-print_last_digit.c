90218a77acc0f2365cb01bd0146fac0ba61f05d7
mosesgitonga <infosec947@gmail.com>
2023-02-21 09:45:40 -0800

    task7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_last_digit - print last
+ * digit 
+ *
+ * @x: kbskks
+ * Return: 0
+ */
+int print_last_digit(int n)
+{
+	int l;
+
+	l = n % 10; 
+	if (l < 0)
+	{
+		l = l * -1;
+	}
+	_putchar(l + '0');
+	return (l);
+}
