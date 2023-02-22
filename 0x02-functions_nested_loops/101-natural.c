716d9a0f924c908a6d9575c150c1fa36100ec8c6
mosesgitonga <infosec947@gmail.com>
2023-02-22 10:06:29 -0800

    multiples-addition

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * multiples - adds multiples of 3 & 5
+ * @n:range to be executed
+ * Return: sum
+ */
+int multiples(int n)
+{
+	int i, sum = 0;
+
+	for (i = 0; i <= n; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+		{
+			sum = sum + i;
+		}
+	}
+	return (sum);
+}
+/**
+ * main - executes function
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int result;
+
+	result = multiples(9);
+	printf("%d\n", result);
+	return (0);
+}
