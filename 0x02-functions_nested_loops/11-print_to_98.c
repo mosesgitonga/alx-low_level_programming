b83d3f9e0a0b32e90922bf3f9559180737612b37
mosesgitonga <infosec947@gmail.com>
2023-02-22 05:55:20 -0800

    dd

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,7 +9,7 @@ void print_to_98(int n)
 {
 	
 
-	while (n > 98)
+	while (n >= 98)
 	{
 		printf("%d", n);
 		if (n != 98)
@@ -30,5 +30,6 @@ void print_to_98(int n)
 		}
 		n++;
 	}
+	printf("\n");
 	
 }
