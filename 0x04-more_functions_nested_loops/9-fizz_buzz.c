a040da2aac08bb0d7e4e9bf5f1ba81d6ddbcc5aa
mosesgitonga <infosec947@gmail.com>
2023-02-23 06:39:20 -0800

    fizibuzi

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -2,7 +2,7 @@
 int main(void)
 {
 	int i;
-	for (i = 1; i <= 100; i++)
+	for (i = 1; i <= 99; i++)
 	{
 		if ((i % 3 == 0) && (i % 5 == 0))
 		{
@@ -22,5 +22,6 @@ int main(void)
 			printf("%d ", i);
 		}
 	}
+	putchar('\n');
 	return (0);
 }
