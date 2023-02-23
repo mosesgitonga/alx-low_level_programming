7e006575b7109f7086f551506314d85c9bcf5f0f
mosesgitonga <infosec947@gmail.com>
2023-02-22 22:18:20 -0800

    finished

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * mul - multiplies two values
+ * @a: int 1
+ * @b: int 2
+ * Return: a or  b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -5,4 +5,5 @@
 void print_number(int n);
 int _isupper(int c);
 int _isdigit(int c);
+int mul(int a, int b);
 #endif
