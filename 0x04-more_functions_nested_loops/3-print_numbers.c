f514aadcc68db585baa9accb218edd56838ee7cc
mosesgitonga <infosec947@gmail.com>
2023-02-22 23:08:54 -0800

    printing-numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,14 +1,18 @@
 #include "main.h"
+/**
+ * print_numbers - prints 1 to 9
+ * @void: no input
+ */
 void print_numbers(void)
 {
 	int i;
 
 	for (i = 0; i <= 9; i++)
 	{
-		
+
 		_putchar(i + '0');
 
 	}
 	_putchar('\n');
-	
+
 }
