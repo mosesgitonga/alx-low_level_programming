57b5114b580c24ab74e75c1430fdef6ab4d74833
mosesgitonga <infosec947@gmail.com>
2023-02-22 23:04:20 -0800

    printing-numbers

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,9 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
-
+int _putchar(char c);
 
 void print_number(int n);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
+void print_numbers(void);
+
 #endif
