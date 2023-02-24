2b8b3f06b84cd93eefbe7694694a38228d9d688f
mosesgitonga <infosec947@gmail.com>
2023-02-24 04:28:47 -0800

    almost commited suicide

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,2 +1,27 @@
 #include "main.h"
+/**
+ * print_diagonal - prints a diagonal line
+ *
+ * @n: size of diagonal
+ */
 void print_diagonal(int n)
+{
+	int i, j;
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+
+	for (i = 1; i <= n; i++)
+	{
+		for (j = 1; j <= i; j++)
+		{
+			_putchar(' ');
+			if (j == i)
+			{
+				_putchar('\\');
+			}
+		}
+		_putchar('\n');
+	}
+}
