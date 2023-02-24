b1f058f0f85e2d94817de6958b2ba84113dedf23
mosesgitonga <infosec947@gmail.com>
2023-02-24 06:36:48 -0800

    i like you

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
+/**
+ * factors - finds factors
+ * main - execution of function
+ * @n: var
+ * Return: 0
+ */
 int factors(int n)
 {
 	int i;
-	
+
 
 	for (i = 2; i <= n; i++)
 	{
@@ -14,13 +20,20 @@ int factors(int n)
 	}
 	printf("\n");
 	return (0);
-	
-	
+
+
 }
-int main()
+/**
+ * main - execution
+ * @void:empty
+ * Return: 0
+ */
+int main(void)
 {
+
 	int result;
+
 	result = factors(1231952);
-	printf("%d",result);
+	printf("%d", result);
 	return (0);
 }
