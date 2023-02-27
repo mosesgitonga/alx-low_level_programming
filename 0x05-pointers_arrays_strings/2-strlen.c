55d930b5ab860f34687958c57fbe3e52b8821296
mosesgitonga <infosec947@gmail.com>
2023-02-27 02:10:48 -0800

    another banger

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+/**
+ * _strlen - finds length of a string
+ * @s: var
+ * Return:length
+ */
+int _strlen(char *s)
+{
+	int length = strlen(s);
+
+	return (length);
+}
