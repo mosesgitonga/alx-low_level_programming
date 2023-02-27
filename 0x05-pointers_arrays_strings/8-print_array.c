8ce1c48045b8202fc6f3d3ba49693d4abd6654d8
mosesgitonga <infosec947@gmail.com>
2023-02-27 10:12:17 -0800

    no suicide

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,12 +1,18 @@
 #include <stdio.h>
 #include "main.h"
 #include <string.h>
+/**
+ * print_array - printing values
+ * in array
+ * @a: array
+ * @n: range
+ */
 
 
 void print_array(int *a, int n)
 {
 	int i;
-	
+
 
 	for (i = 0; i < n; i++)
 	{
