2ae3fece97c89894741a0540739825dd69d36e38
mosesgitonga <infosec947@gmail.com>
2023-02-27 06:52:47 -0800

    commmit

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,9 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
+void puts_half(char *str);
 int _strlen(char *s);
 void _puts(char *str);
 void rev_string(char *s);
-
+void puts2(char *str);
 void print_rev(char *s);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
