4420de5bf2469e7f807f1337d2bbc2cb1c9b1f9e
mosesgitonga <infosec947@gmail.com>
2023-02-27 07:00:22 -0800

    doing

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,7 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
 void puts_half(char *str);
+void print_array(int *a, int n);
 int _strlen(char *s);
+int _atoi(char *s);
+
 void _puts(char *str);
 void rev_string(char *s);
 void puts2(char *str);
