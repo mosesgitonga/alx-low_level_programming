3aeb201d270fb658ac64a5d94f37d290c637a494
mosesgitonga <infosec947@gmail.com>
2023-03-01 04:37:14 -0800

    learn

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,6 +1,6 @@
 #ifndef MAIN_HS
 #define MAIN_HS
-
+char *string_toupper(char *);
 void reverse_array(int *a, int n);
 int _strcmp(char *s1, char *s2);
 char *_strncat(char *dest, char *src, int n);
