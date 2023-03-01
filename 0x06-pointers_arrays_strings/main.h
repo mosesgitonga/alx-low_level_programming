f3bdee27ad8b6d997b69c4c1fe7ed36c7765ec9a
mosesgitonga <infosec947@gmail.com>
2023-03-01 02:12:24 -0800

    should i commit suicide

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,5 +1,7 @@
 #ifndef MAIN_HS
 #define MAIN_HS
+
+void reverse_array(int *a, int n);
 int _strcmp(char *s1, char *s2);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
