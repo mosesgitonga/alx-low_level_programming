1fd2e357c264fb734126a52f83614a4ba143ee98
mosesgitonga <infosec947@gmail.com>
2023-03-01 00:36:29 -0800

    doingHardThings

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,5 +1,6 @@
 #ifndef MAIN_HS
 #define MAIN_HS
+int _strcmp(char *s1, char *s2);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
 char *_strcat(char *dest, char *src);
