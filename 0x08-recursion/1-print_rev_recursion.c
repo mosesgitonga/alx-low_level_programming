00aa3872c1ce43f84ef150b1c0121e1f93bfe02b
mosesgitonga <infosec947@gmail.com>
2023-03-08 01:32:31 -0800

    core-dumbrd

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,5 +1,22 @@
 #include "stdio.h"
 #include "main.h"
+#include <string.h>
+#include <stdlib.h>
+void _print_rev_recursion(char *s)
 {
-		
+	if (s != NULL)
+	{
+		char *save = malloc(strlen(s) + 1);
+		if (save == NULL)
+		{
+			puts("error in memory allocation");
+			return;
+		}
+		{
+			strcpy(save, s);
+			_print_rev_recursion(save + 1);
+			_putchar(*s);
+			free(save);
+		}
+	}
 }
