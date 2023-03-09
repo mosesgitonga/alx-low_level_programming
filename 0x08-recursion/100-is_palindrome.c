4762a89948c1149fe451aff4be4cde8dce612682
mosesgitonga <infosec947@gmail.com>
2023-03-08 21:48:32 -0800

    suicide

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include  <string.h>
+#include "main.h"
+/**
+ * is_palindrome - function that returns 1 if a string is a palindrome and 0
+ * @n: num
+ * @i: num
+ * Return: something
+ */
+int helper(int n, int i);
+
+int is_palindrome(char *s)
+{
+return (helper(s, i));
+}
+/**
+ *helper - helps is_palindrome
+ *@s: num
+ *@i: num
+ *Return: something
+ */
+int helper(int *s, int i)
+{
+	int len = strlen(s);
+
+	if (len == 0 || len == 1)
+	{
+	return (1);
+	{
+	if ((s[0]) == (s[len - 1]))
+	{
+	return (is_palindrome((s + 1) && len - 2));
+	}
+	return (0);
+}
