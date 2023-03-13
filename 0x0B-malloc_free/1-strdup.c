f52ecb03b482dcbc9c0413e8f2d0673529925eeb
mosesgitonga <infosec947@gmail.com>
2023-03-13 05:37:57 -0700

    duplicate

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+/**
+ * _strdup - prints duplicate
+ * @str: string
+ * Return: dup
+ */
+char *_strdup(char *str)
+{
+	char *dup;
+	size_t len = sizeof(str);
+
+	if (str == NULL)
+		return (NULL);
+
+
+	dup = malloc(len + 1);
+	if (dup != NULL)
+		memcpy(dup, str, len + 1);
+	return (dup);
+}
