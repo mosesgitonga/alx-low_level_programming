6992fd5847e7ab9514b0e619bfd4d0aa30aafa2e
mosesgitonga <infosec947@gmail.com>
2023-03-16 02:41:57 -0700

    check if malloc

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,14 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "main.h"
-
+/**
+ * malloc_checked - to check i f is malloc or not
+ * @b: int
+ * Return: p
+ */
 void *malloc_checked(unsigned int b)
 {
 	void *p;
+
 	p = malloc(b);
+
 	if (p != NULL)
 	{
-		return(p);
+		return (p);
 	}
 	exit(98);
 }
