ee15bd214dbecc741ad4e8769ebc60c727232698
mosesgitonga <infosec947@gmail.com>
2023-03-22 03:37:32 -0700

    updated version to avoid undefined behavior and crashes when passing invalid pointers to the function.

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -12,6 +12,10 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t  i = 1;
 
+	if (array == NULL || action == NULL)
+	{
+		return;
+	}
 	for (i = 0; i < size; i++)
 	{
 		action(array[i]);
