71e6700a4cab55fc8749e270e736eb303c20d075
mosesgitonga <infosec947@gmail.com>
2023-03-30 05:06:10 -0700

    singly linked list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "lists.h"
+
+size_t print_list(const list_t *h)
+{
+	size_t count = 0;
+	const list_t  *current = h;
+
+	while (current != NULL)
+	{
+		if (current->str == NULL)
+		{
+			printf("[0] (nil)\n");
+			count++;
+		}
+		else
+		{
+			printf("[%d] %s\n", current->len, current->str);
+			count++;
+		}
+		current = current->next;
+	}
+
+	return (count);
+}
