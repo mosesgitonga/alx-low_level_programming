62799ab7ed440eabc1c343309d9fb1583228fb63
mosesgitonga <infosec947@gmail.com>
2023-04-04 09:55:54 +0300

    function that prints a listint_t linked list.

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ *print_listint_safe - prints a singly list once
+ * @head: head
+ *Return: size_t
+ */
+size_t print_listint_safe(const listint_t *head)
+{
+	const listint_t *temp, *current_Node;
+	size_t count = 0;
+
+	current_Node = head;
+
+	while (current_Node->next != NULL)
+	{
+		printf("[%p] %d\n", (void *)current_Node->next, current_Node->n);
+		count++;
+		temp = current_Node;
+		current_Node = current_Node->next;
+		if (temp <= current_Node)
+		{
+			printf("-> [%p] %d\n", (void *)current_Node->next, current_Node->n);
+			exit(98);
+		}
+
+	}
+	return (count);
+
+}
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -16,11 +16,11 @@ typedef struct listint_s
     int n;
     struct listint_s *next;
 } listint_t;
-
+size_t print_listint_safe(const listint_t *head);
 size_t listint_len(const listint_t *h);
 size_t print_listint(const listint_t *h);
 void free_listint2(listint_t **head);
-
+int delete_nodeint_at_index(listint_t **head, unsigned int index);
 listint_t *add_nodeint_end(listint_t **head, const int n);
 int pop_listint(listint_t **head);
 listint_t *add_nodeint(listint_t **head, const int n);
