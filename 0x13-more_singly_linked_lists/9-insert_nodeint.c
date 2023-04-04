8375e1182c6de821f7763b75d32cbe7935cf3721
mosesgitonga <infosec947@gmail.com>
2023-04-04 10:24:25 -0700

    requirement output fix

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -43,5 +43,9 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		}
 		currentNode = currentNode->next;
 	}
+	if (!temp)
+	{
+		return (NULL);
+	}
 	return (NULL);
 }
