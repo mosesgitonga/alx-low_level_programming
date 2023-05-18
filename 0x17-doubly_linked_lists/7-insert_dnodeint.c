51d75c290af80cf544dbde4e114ffd2da3dcab92
mosesgitonga <infosec947@gmail.com>
2023-05-18 05:01:28 -0700

    coditiion

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -27,6 +27,10 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	{
 		if (count == idx)
 		{
+			if (idx > count)
+			{
+				return (NULL);
+			}
 			if (tmp->prev != NULL)
 			{
 				tmp->prev->next = newnode;
