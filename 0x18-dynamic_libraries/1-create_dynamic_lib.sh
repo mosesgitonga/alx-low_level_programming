815cb7aa1fda16d8569ef94ecec3d87fc13c5a8b
mosesgitonga <infosec947@gmail.com>
2023-05-26 01:02:41 -0700

    .o s

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-
-
 gcc -Wall -Wextra -Werror -pedantic -c -fPIC 5-strstr.c
 gcc -Wall -Wextra -Werror -pedantic -c -fPIC 4-strpbrk.c
 gcc -Wall -Wextra -Werror -pedantic -c -fPIC 3-strspn.c
@@ -19,7 +17,6 @@ gcc -Wall -Wextra -Werror -pedantic -c -fPIC 3-islower.c
 gcc -Wall -Wextra -Werror -pedantic -c -fPIC 4-isalpha.c
 gcc -Wall -Wextra -Werror -pedantic -c -fPIC 6-abs.c
 
-gcc -shared -o liball.so 0-putchar.o 2-strlen.o 2-strncpy.o 0-isupper.o 0-strcat.o 100-atoi.o 1-strncat.o 3-islower.o 4-isalpha.o 6-abs.o  5-strstr.c 4-strpbrk.c 3-strspn.c 2-strchr.c 1-memcpy.c 0-memset.c
-rm *.o
+gcc -shared -o liball.so 0-putchar.o 2-strlen.o 2-strncpy.o 0-isupper.o 0-strcat.o 100-atoi.o 1-strncat.o 3-islower.o 4-isalpha.o 6-abs.o  5-strstr.o 4-strpbrk.o 3-strspn.o 2-strchr.o 1-memcpy.o 0-memset.o
 export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
 
